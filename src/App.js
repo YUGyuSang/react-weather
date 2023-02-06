af5e6c8b5fdea1bbd3a5d85e059567ded120a113
gyusang <monet000414@naver.com>
2023-02-06 16:59:41 +0900

    ui 작업

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import { useEffect } from 'react';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import WeatherBox from './component/WeatherBox';
+import WeatherButton from './component/WeatherButton';
 // 1. 앱이 실행되자마자 현재위치기반의 날씨가 보인다.
 // 2. 날씨정보에는 도시, 섭씨 화씨 날씨상태
 // 3. 5개의 버튼이 있다 (1개는 현재 위치 4개는 다른 도시)
@@ -22,15 +24,19 @@ function App() {
     let response = await fetch(url)
     let data = await response.json(); // json을 사용하는 이유는 fetch로 받으면 바로 데이터를 사용 할 수 없기 때문에 json으로 마무리를 해주어 데이터를 뿌려줄 수 있다.
     console.log("data: ", data)
-  } 
-
+    console.log("name: ", data.name);
+  }
+  
   useEffect(()=>{
     getCurrentLocation() // 실행되자마다 현재위치 정보를 가져와야하기 때문에 useEffect에 적용한다.
   },[])
 
   return (
     <div>
-      
+      <div className='container'>
+        <WeatherBox />
+        <WeatherButton />
+      </div>
     </div>
   );
 }
