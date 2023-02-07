a2df54575f967dde2db96b944494bedf9820dcb8
gyusang <monet000414@naver.com>
2023-02-07 21:12:43 +0900

    날씨데이터가져오기-2

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import './App.css';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, CSSProperties  } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import WeatherBox from './component/WeatherBox';
 import WeatherButton from './component/WeatherButton';
+import ClipLoader from "react-spinners/ClipLoader";
 // 1. 앱이 실행되자마자 현재위치기반의 날씨가 보인다.
 // 2. 날씨정보에는 도시, 섭씨 화씨 날씨상태
 // 3. 5개의 버튼이 있다 (1개는 현재 위치 4개는 다른 도시)
@@ -12,6 +13,28 @@ import WeatherButton from './component/WeatherButton';
 function App() {
 
   const [weather,setWeather] = useState(null);
+  const cities = ['paris','new york', 'tokyo', 'seoul'];
+  const [city,setCity]=useState("");
+  const [loading,setLoading] = useState(false);
+ 
+
+  const getWeatherByCity= async()=>{
+    setLoading(true)
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=1aa3253a785fd768b157ee07b30643f0&units=metric`
+    let response = await fetch(url)
+    let data = await response.json()
+    console.log("getweatherbucity data?", data);
+    setWeather(data);
+    setLoading(false)
+  }
+
+  const handleCityChange = (city) => {
+    if (city == "current") {
+      setCity(null);
+    } else {
+      setCity(city);
+    }
+  };
 
   const getCurrentLocation =()=>{
     navigator.geolocation.getCurrentPosition((position)=>{ // 익명 함수를 주어 만들었다.
@@ -24,23 +47,32 @@ function App() {
 
   const getWeatherByCurrentLocation = async(lat, lon) =>{ //비동기적으로 처리
     let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=1aa3253a785fd768b157ee07b30643f0&units=metric`;
+    setLoading(true)
     let response = await fetch(url)
     let data = await response.json(); // json을 사용하는 이유는 fetch로 받으면 바로 데이터를 사용 할 수 없기 때문에 json으로 마무리를 해주어 데이터를 뿌려줄 수 있다.
     setWeather(data);
+    setLoading(false)
     console.log("data: ", data)
     console.log("name: ", data.name);
   }
   
   useEffect(()=>{
-    getCurrentLocation() // 실행되자마다 현재위치 정보를 가져와야하기 때문에 useEffect에 적용한다.
-  },[])
+    if(city==""){
+      getCurrentLocation()// 실행되자마다 현재위치 정보를 가져와야하기 때문에 useEffect에 적용한다.
+    }else{
+      getWeatherByCity()
+    }
+  },[city])
 
   return (
     <div>
+      
+      {loading? <div className='container'> <ClipLoader color={'#f88c6b'} loading={loading} size={150}/></div> : 
       <div className='container'>
-        <WeatherBox weather={weather}/>
-        <WeatherButton />
-      </div>
+      <WeatherBox weather={weather}/>
+      <WeatherButton cities = {cities} setCity={setCity} handleCityChange={handleCityChange} />
+    </div>}
+      
     </div>
   );
 }
--- src/component/WeatherButton.js
+++ src/component/WeatherButton.js
@@ -2,12 +2,15 @@ import React from 'react'
 import { Button } from 'react-bootstrap';
 
 
-const WeatherButton = () => {
+const WeatherButton = ({cities,setCity,handleCityChange}) => {
+    console.log("cities?", cities)
   return (
-    <div>
-      <Button variant="warning">Current Loaction</Button>
-      <Button variant="warning">paris</Button>
-      <Button variant="warning">new york</Button>
+    <div  class="menu-container">
+      <Button variant="warning" onClick={()=>handleCityChange("current")}>Current Loaction</Button>
+      
+      {cities.map((item)=>(
+        <Button variant={`${setCity == item ? "outline-warning" : "warning"}`} onClick={()=>setCity(item)}>{item}</Button>
+      ))}
     </div>
   )
 }
